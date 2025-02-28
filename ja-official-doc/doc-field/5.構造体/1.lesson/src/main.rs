4088a2092435b503c1b0ce12dbfa62e42b6e59e8
k_95.21 <kohta0421kochi@gmail.com>
2025-02-28 20:09:52 +0900

    add new struct lecture

---
+++ ja-official-doc/doc-field/5.構造体/1.lesson/src/main.rs
@@ -0,0 +1,52 @@
+fn main() {
+    println!("Hello, world!");
+
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        ..user1
+    };
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        active: user1.active,
+        sign_in_count: user1.sign_in_count,
+    };
+
+    user1.email = String::from("anothermail@example.com");
+
+    println!("{}", user1.email);
+
+    // タプル構造体
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+// fn build_ser(email: String, username: String) -> User {
+//     User {
+//         email: email,
+//         username: userame,
+//         active: true,
+//         sign_in_count: 1,
+//     }
+// }
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
